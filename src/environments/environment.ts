290457173e3e8405e52daaad42583da418a19a8d
instructor4demo <instructor.4demo@gmail.com>
2024-09-27 14:45:30 +0900

    Add supabase auth feature

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    supabaseUrl: 'YOUR_SUPABASE_URL',
+    supabaseAnonKey: 'YOUR_SUPABASE_ANON_KEY',
+    signUpRedirectUrl: 'http://localhost:4200/login',
+    resetPasswordRedirectUrl: 'http://localhost:4200/reset-password'
+};
